91db6a85d06224ab74364a11680dbd6b476494fc
chia <chanchia@me.com>
2017-07-07 09:51:12 +0800

    Added Project page.

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
 	});
 })
 
+app.get('/project', (req, res) => {
+	res.render('project.hbs', {
+		pageTitle: 'Project'
+	});
+})
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Handling error'
